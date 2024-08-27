0914364e04852c703a29b75eb9b123d9c6f478c7
MrWais001 <abdulgafarridwan@gmail.com>
2024-08-27 13:40:16 -0700

    final

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,47 +1,28 @@
 #!/usr/bin/env python3
-"""
-Force locale with URL parameter
-"""
-
-import babel
 from flask import Flask, render_template, request
-from flask_babel import Babel
-
-app = Flask(__name__)
-babel = Babel(app)
-
+from flask_babel import Babel, _
 
 class Config:
-    """
-    Config class
-    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
+app = Flask(__name__)
 app.config.from_object(Config)
 
+babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """
-    detect if the incoming request contains locale
-    argument and ifs value is a supported locale, return it
-    """
     locale = request.args.get('locale')
-    if locale:
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/')
 def index():
-    """
-    hello world
-    """
     return render_template('4-index.html')
 
+if __name__ == "__main__":
+    app.run()
 
-if __name__ == '__main__':
-    app.run(debug=True)
--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,26 +1,16 @@
-i#!/usr/bin/env python3
-
-"""
-5. Basic Flask app
-"""
-
+#!/usr/bin/env python3
 from flask import Flask, render_template, request, g
-from flask_babel import Babel
-
-app = Flask(__name__)
-babel = Babel(app)
-
+from flask_babel import Babel, _
 
 class Config:
-    """
-    Config class.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
+app = Flask(__name__)
 app.config.from_object(Config)
+
+babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -29,43 +19,29 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-
-def get_user(login_as):
-    """
-    get_user.
-    """
-    try:
-        return users.get(int(login_as))
-    except Exception:
-        return
-
+def get_user():
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
 
 @app.before_request
 def before_request():
-    """
-    before_request
-    """
-    g.user = get_user(request.args.get("login_as"))
-
+    g.user = get_user()
 
 @babel.localeselector
 def get_locale():
-    """
-    get_locale.
-    """
-    locale = request.args.get("locale")
-    if locale:
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
         return locale
+    if g.user and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
-@app.route('/', methods=["GET"], strict_slashes=False)
-def hello():
-    """
-    hello.
-    """
+@app.route('/')
+def index():
     return render_template('5-index.html')
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
+
